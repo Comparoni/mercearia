268aba9a1c5293dea5e49f60659eb226e33916cf
Bianca <btcomparoni@gmail.com>
2018-03-31 06:10:48 -0300

    Rotas e URLs amigáveis

---
+++ model/Rotas.class.php
@@ -0,0 +1,22 @@
+<?<?php 
+
+
+Class Rotas{
+
+	static function get_Pagina(){
+		if(isset($_GET['pag'])){
+			$pagina = 'controller/' .$_GET['pag'].'.php';
+			if(file_exists($pagina)){
+				include $pagina;				
+			}else{
+			include 'erro.php';
+		}
+		
+		}
+	} 
+
+}
+
+
+
+ ?>
